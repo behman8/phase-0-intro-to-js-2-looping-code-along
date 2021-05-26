c91ccbd1559b6cca6f9a149327396fbbc94362b2
Branden <brandenehman45@gmail.com>
2021-05-26 10:19:37 -0600

    commit tree f36ca2078b0a9324a8fc99eb86add9cf1aaacab9

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const list = [ 'Lisa', 'Kaitlin', 'Jan' ]
 function writeCards( guests, surprise ) {
     for (let i = 0; i < guests.length; i++) {
         guests[i] = (`Thank you, ${guests[i]}, for the wonderful ${surprise} gift!`);
-        debugger;
     }
     return guests;
 }
 
 function countDown(number) {
-    while (number > -1) {
-        console.log(number);
-        number--;
+    while (number >= 0) {
+        console.log(number--);
     }
 }
